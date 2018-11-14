937d087faa40d24cc34076d29a918562ba98fbfa
kanoonsantikul <nitisantikul@gmail.com>
2018-11-15 03:49:25 +0700

    k-mean - implement k-mean + change crawler to collect digit as well

--- src/facebook_data.py
+++ src/facebook_data.py
@@ -1,26 +1,27 @@
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString
 
 import os
 import re
 
 def clean(doc):
     while True:
-        new_doc = re.sub('[^\u0E00-\u0E7F]+', '', doc)
+        new_doc = re.sub('[^" "0-9\u0E00-\u0E7F]+', '', doc)
         if doc == new_doc:
             break
         else:
             doc = new_doc
 
-    return doc
+    return doc.strip()
 
-with open('../data/facebook/2/ผู้บริโภค - VillaMarket.html') as html:
+with open('all-cafe.html') as html:
     soup = BeautifulSoup(html, 'html.parser')
 
 comment_container = soup.find('div', {'class': 'fbPhotosSnowliftFeedback'})
 comment_container = comment_container.find('div', {'class': 'UFIList'})
 comments = comment_container.find_all('div', {'class': 'UFICommentActorAndBodySpacing'})
 
-file_name = '../data/facebook/ผู้บริโภค - VillaMarket.txt'
+file_name = './all-cafe.txt'
 os.makedirs(os.path.dirname(file_name), exist_ok=True)
 
 with open(file_name, 'w') as f:
@@ -28,7 +29,13 @@ with open(file_name, 'w') as f:
 
     for i, comment in enumerate(comments):
         comment = comment.find('span', {'class': 'UFICommentBody'})
+        comment_children = comment.descendants 
+        comment = ''
+        for child in comment_children:
+        	if isinstance(child, NavigableString):
+        		comment += child
+        		
         comment = clean(str(comment))
-        if comment == '':
+        if comment == '' or comment.isdigit():
             continue
         f.write('comment' + str(i) + '::' + comment + '\n')

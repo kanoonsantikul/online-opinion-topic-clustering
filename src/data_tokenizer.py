9ddfb8987d4d857d216cb806a936015d01b3f08e
kanoonsantikul <nitisantikul@gmail.com>
2018-12-12 19:22:18 +0700

    add module to store tokenized data + rename some files to more meaningful context

---
+++ src/data_tokenizer.py
@@ -0,0 +1,59 @@
+import re
+from pythainlp.tokenize import word_tokenize
+
+def load_corpus(file_directory):
+	corpus = []
+	labels = []
+	    
+	f = open(file_directory, 'r')
+	data = f.read().splitlines()
+	f.close()
+    
+	start = False
+	num = -2
+
+	for line in data:
+		if line.startswith('comment'):
+			num += 1
+			if num < 0:
+				continue
+
+			comment = ''.join(x for x in line.split(':')[2:])
+			corpus.insert(num, comment)
+			labels.insert(num, line.split(':')[1])
+			start = True
+
+		elif start:
+			corpus[num] += line
+	
+	return corpus, labels
+
+def clean(doc):
+	doc.strip()
+	while True:
+		new_doc = re.sub('[^" "\u0E00-\u0E7F]+', '', doc)
+		if doc == new_doc:
+			break
+		else:
+			doc = new_doc
+	return doc
+
+def main():
+	file_name = 'ผู้บริโภค - TescoLotus.txt'
+	corpus = load_corpus('../data/facebook/' + file_name)[0]
+	print('Total documents', len(corpus))
+
+	with open('../data/facebook/tokenized/tokenized_' + file_name, 'w') as f:
+		f.write('[')
+		for doc in corpus:
+			cleaned_doc = clean(doc)
+			print(cleaned_doc)
+			tokenized_doc = word_tokenize(cleaned_doc, engine='deepcut')
+			f.write('[')
+			for word in tokenized_doc:
+				f.write('\'' + word + '\',')
+			f.write('],')
+		f.write(']')
+		
+if __name__== "__main__":
+  main()
